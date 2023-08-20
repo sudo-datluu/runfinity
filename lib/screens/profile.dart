b6fac11d81489e4cf630e610b93bb8f12f5de73a
Pham Ngoc Minh <ngocminhpham.starack@gmail.com>
2023-08-20 17:37:54 +1000

    .

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:runfinity/styles/app_colors.dart';
+import 'package:runfinity/widget/appText.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -15,9 +16,39 @@ class _ProfilePageState extends State<ProfilePage> {
       body: Container(
         color: AppColors.neutral700,
         padding: const EdgeInsets.symmetric(horizontal: 20),
-       child: SafeArea(child: ),
-        ),
-      );
-    
+        child: SafeArea(
+            child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Image.asset('images/backButton.png'),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    AppText(
+                      text: 'User',
+                      fontWeight: FontWeight.w700,
+                      size: 18,
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset('images/direct-noti.png'),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Image.asset('images/sms-noti.png'),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        )),
+      ),
+    );
   }
 }
