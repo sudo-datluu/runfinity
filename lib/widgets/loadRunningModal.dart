04ecb50bbfd0c03809d20022001a94d1d1dff155
Pham Ngoc Minh <ngocminhpham.starack@gmail.com>
2023-08-24 08:12:09 +1000

    feat: [UTS-46] loading-run-modal

--- lib/widgets/loadRunningModal.dart
+++ lib/widgets/loadRunningModal.dart
@@ -12,40 +12,38 @@ class LoadRunningModal extends StatelessWidget {
     return Visibility(
       visible: isLoadRun!,
       child: Scaffold(
-        body: Container(
-            color: Colors.black.withOpacity(0.3),
-            child: Center(
-                child: Container(
-                    width: 327,
-                    height: 288,
-                    padding: const EdgeInsets.symmetric(horizontal: 50),
-                    decoration: BoxDecoration(
-                        color: AppColors.neutral800,
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(color: AppColors.primary)),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                            width: 160,
-                            height: 144,
-                            child: Image.asset('images/loadRun.png')),
-                        const Text(
-                          'Preparing your run...',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 22,
-                              fontWeight: FontWeight.w800),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        const Text(
-                          'Our system will track you real-time performance.',
-                          style: TextStyle(color: AppColors.neutral400),
-                        ),
-                      ],
-                    )))),
+        body: Center(
+            child: Container(
+                width: 327,
+                height: 288,
+                padding: const EdgeInsets.symmetric(horizontal: 50),
+                decoration: BoxDecoration(
+                    color: AppColors.neutral700,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: AppColors.primary)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                        width: 160,
+                        height: 144,
+                        child: Image.asset('images/loadRun.png')),
+                    const Text(
+                      'Preparing your run...',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w800),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      'Our system will track you real-time performance.',
+                      style: TextStyle(color: AppColors.neutral400),
+                    ),
+                  ],
+                ))),
       ),
     );
   }
