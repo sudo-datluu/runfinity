d0c63d4793827a33e5a6b9174e54be77715c1f6d
Thanh Nhan <85187276+NhanVo-gmt@users.noreply.github.com>
2023-09-04 18:18:02 +1000

    Adding lobby controller

---
+++ lib/controllers/auth/lobby_controller.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class Lobby_Controller extends GetxController {
+  TextEditingController locationAddressEditingController = TextEditingController();
+  TextEditingController limitMemberEditingController = TextEditingController();
+  TextEditingController lobbyNameEditingController = TextEditingController();
+
+  Future<void> LoginService() async {
+    try {
+      final body = {
+        ""
+      }
+    } catch {
+
+    }
+  }
+}
