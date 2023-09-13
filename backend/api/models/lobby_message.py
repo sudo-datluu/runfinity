b7e457b1e08326038c05bbc209505aadb55c72e7
Pham Ngoc Minh <ngocminhpham.starack@gmail.com>
2023-09-13 11:34:15 +1000

    [UTS-59] undone api get message

--- backend/api/models/lobby_message.py
+++ backend/api/models/lobby_message.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from api.models.runner import Runner
 
 class LobbyMessage(models.Model):
-    text = models.CharField(max_length=255)
+    class Meta:
+        db_table = 'lobby_message'
+        verbose_name = _('LobbyMessage')
+        verbose_name_plural = _('LobbyMessages')
+
     user = models.ForeignKey(Runner, on_delete=models.CASCADE)
+    content = models.CharField(max_length=255)
     createdAt = models.DateTimeField(auto_now_add=True)
